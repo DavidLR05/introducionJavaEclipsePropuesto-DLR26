03ae06beced020bd9cdc952069774d7ef8089e18
José Gaspar Sánchez García <jg.sanchezgarcia@edu.gva.es>
2026-03-04 17:51:20 +0100

    Ejercicios propuestos

---
+++ src/main/java/ejercicios/Ejercicio024.java
@@ -0,0 +1,16 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio024 {
+	
+	//COMPLETAR METODO
+	public void calculadoraNotas() {
+        
+    }
+
+    public static void main(String[] args) {
+        Ejercicio024 ejercicio = new Ejercicio024();
+        ejercicio.calculadoraNotas();
+    }
+}
---
+++ src/main/java/ejercicios/Ejercicio035.java
@@ -0,0 +1,17 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio035 {
+	
+	//COMPLETAR METODO
+	public void calcularMediaArray() {
+        
+    }
+    
+    public static void main(String[] args) {
+        Ejercicio035 ejercicio = new Ejercicio035();
+        ejercicio.calcularMediaArray();
+    }
+    
+}
